765ad92b705a2d4a96e9960e681ab566397c74e7
Jaime Navarrete <jaimenava123@outlook.com>
2023-12-17 00:51:34 -0600

    Crea proyecto MVC WebApp y archivo solución

---
+++ PruebaTecnica.WebApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaTecnica.WebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PruebaTecnica.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PruebaTecnica.WebApp.MvcApplication" Language="C#" %>
