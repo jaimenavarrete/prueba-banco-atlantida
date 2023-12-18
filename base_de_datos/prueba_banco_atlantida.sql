c95819bf322ef384f2b8b01b5f66f5876e3ae425
Jaime Navarrete <jaimenava123@outlook.com>
2023-12-18 17:01:40 -0600

    Agrega insercion de datos iniciales en tablas de base de datos
    Inserta datos en tablas de cuenta y configuraciones.

--- base_de_datos/prueba_banco_atlantida.sql
+++ base_de_datos/prueba_banco_atlantida.sql
@@ -72,3 +72,25 @@ AS
 
 	UPDATE Cuentas SET SaldoTotal = SaldoTotal - @Monto
 	WHERE Id = @CuentaId
+GO
+
+-- INSERTAR DATOS INICIALES
+
+-- Tabla de cuentas
+INSERT INTO Cuentas (Id, NombreTitular, NumeroTarjeta, SaldoTotal, LimiteCredito)
+VALUES (
+	'454a540a-f481-430c-a7ce-6a1516732ff4', 
+	'Jaime Eduardo Navarrete Cubías',
+	'r9WS1uT6X+/q4uEQBC7Mng==9XMMzokva8CzA3dIb52ICFCmWLX7Aj6LQgB5v7+rmS8=',
+	0,
+	1000);
+GO
+
+-- Tabla de configuraciones
+INSERT INTO Configuraciones (Id, PorcentajeInteres, PorcentajeSaldoMinimo)
+VALUES (
+	'be1508e8-b34c-4dbb-90de-b540b1e708c4',
+	0.05,
+	0.20
+);
+GO
