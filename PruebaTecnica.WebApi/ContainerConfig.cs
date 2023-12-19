2d180528dd6e7577ff1ee3e6090bcceea987b8e8
Jaime Navarrete <jaimenava123@outlook.com>
2023-12-19 17:34:07 -0600

    Implementa contenedor de inyección de dependencias con Autofac

---
+++ PruebaTecnica.WebApi/ContainerConfig.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using System.Web.Http;
+
+namespace PruebaTecnica.WebApi
+{
+    public static class ContainerConfig
+    {
+        private static ContainerBuilder RegisterDependencies(ContainerBuilder builder)
+        {
+            return builder;
+        }
+
+        public static void Configure(HttpConfiguration configuration)
+        {
+            var builder = new ContainerBuilder();
+
+            builder = RegisterDependencies(builder);
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+
+            configuration.DependencyResolver = resolver;
+        }
+    }
+}
--- PruebaTecnica.WebApi/Global.asax.cs
+++ PruebaTecnica.WebApi/Global.asax.cs
@@ -9,6 +9,9 @@ namespace PruebaTecnica.WebApi
     {
         protected void Application_Start()
         {
+            // Configuración de Inyección de Dependencias
+            ContainerConfig.Configure(GlobalConfiguration.Configuration);
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
