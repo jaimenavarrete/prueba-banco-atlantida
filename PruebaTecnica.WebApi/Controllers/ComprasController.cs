900ca50cd208d517309bf7dcc48eb371727ab1e9
Jaime Navarrete <jaimenava123@outlook.com>
2023-12-20 02:08:09 -0600

    Crea endpoints para insertar compras y pagos

---
+++ PruebaTecnica.WebApi/Controllers/ComprasController.cs
@@ -0,0 +1,25 @@
+﻿using PruebaTecnica.WebApi.Core.Entities;
+using PruebaTecnica.WebApi.Core.Interfaces.Services;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PruebaTecnica.WebApi.Controllers
+{
+    public class ComprasController : ApiController
+    {
+        private readonly IComprasService _comprasService;
+
+        public ComprasController(IComprasService comprasService)
+        {
+            _comprasService = comprasService;
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> InsertarCompra(Compra compra)
+        {
+            var compraCreada = await _comprasService.InsertarCompra(compra);
+
+            return Created("", compraCreada);
+        }
+    }
+}
---
+++ PruebaTecnica.WebApi/Controllers/PagosController.cs
@@ -0,0 +1,25 @@
+﻿using PruebaTecnica.WebApi.Core.Entities;
+using PruebaTecnica.WebApi.Core.Interfaces.Services;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PruebaTecnica.WebApi.Controllers
+{
+    public class PagosController : ApiController
+    {
+        private readonly IPagosService _pagosService;
+
+        public PagosController(IPagosService pagosService)
+        {
+            _pagosService = pagosService;
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> InsertarCompra(Pago pago)
+        {
+            var pagoCreado = await _pagosService.InsertarPago(pago);
+
+            return Created("", pagoCreado);
+        }
+    }
+}
