59978c5deca900aaa1d4d79e71c90a0e3488054e
Jaime Navarrete <jaimenava123@outlook.com>
2023-12-19 19:48:33 -0600

    Renombra controlador values a cuentas. Agrega endpoint para obtener estado cuenta

---
+++ PruebaTecnica.WebApi/Controllers/CuentasController.cs
@@ -0,0 +1,26 @@
+﻿using PruebaTecnica.WebApi.Core.Entities;
+using PruebaTecnica.WebApi.Core.Interfaces.Services;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PruebaTecnica.WebApi.Controllers
+{
+    public class CuentasController : ApiController
+    {
+        private readonly ICuentasService _cuentasService;
+
+        public CuentasController(ICuentasService cuentasService)
+        {
+            _cuentasService = cuentasService;
+        }
+
+        [Route("api/cuenta/estadoCuenta")]
+        [HttpGet]
+        public async Task<EstadoCuenta> ObtenerEstadoCuenta()
+        {
+            var estadoCuenta = await _cuentasService.ObtenerEstadoCuenta();
+
+            return estadoCuenta;
+        }
+    }
+}
